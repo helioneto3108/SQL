9c324565d64be7fb96748ec00c18623927257d36
Helio Tavares de Sá Neto <heliotavares3108@outlook.com>
2024-07-14 15:06:09 +0100

    Filtro aplicado na tabela para vendas de 2017

--- Olist_Test_TMW/Dia04/join_groupby_where.sql
+++ Olist_Test_TMW/Dia04/join_groupby_where.sql
@@ -1,5 +1,6 @@
 /* - Qual a receita de cada categoria por produto?
-- e o total de vendas? em unidades e pedidos */
+- e o total de vendas? em unidades e pedidos 
+- Considere as vendas de 2017 e pedidos entregues */
 
 select
         t2.product_category_name,
@@ -12,6 +13,12 @@ from tb_order_items as t1
 
 left join tb_products as t2
     on t1.product_id = t2.product_id
+
+left join tb_orders as t3
+    on t1.order_id = t3.order_id
+
+where t3.order_status = 'delivered'
+    and cast(strftime('%Y', t3.order_approved_at) as int) = 2017
 
 group by t2.product_category_name
 
