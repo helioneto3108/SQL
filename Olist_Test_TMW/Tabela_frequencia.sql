0072376d3e787d1efe5fd7deaad4469d3237675b
Helio Tavares de Sá Neto <heliotavares3108@outlook.com>
2024-07-23 18:18:31 +0100

    Análise reformulada

--- Olist_Test_TMW/Tabela_frequencia.sql
+++ Olist_Test_TMW/Tabela_frequencia.sql
@@ -1,3 +1,19 @@
-select *
+with tb_freq_abs as (
+    select seller_state,
+        count(*) as freq_abs
 
-from tb_sellers
+    from tb_sellers
+
+    group by seller_state
+)
+
+select *,
+    cast(freq_abs as float) / (select sum(freq_abs) from tb_freq_abs) as freq_relativa,
+    sum(cast(freq_abs as float) / (select sum(freq_abs) from tb_freq_abs)) 
+        over (order by freq_abs desc) as freq_relativa_acumulada
+
+from tb_freq_abs
+
+group by seller_state
+
+order by freq_abs desc
